cb6ac0c77ed2ac2c661d1304c78c2516a78567d5
Jonas Emil Andersen <jonas@bluesteel.dk>
2015-06-12 08:57:34 +0200

    new stuff

--- Countdown.js
+++ Countdown.js
@@ -3,25 +3,47 @@ function Countdown(seconds, endText, onlySeconds, hideWhenZero, callback) {
 	var paused = false;
 	var running = true;
 	var startTime = now();
+	var seconds = seconds;
 	var $countdown= $('<ib />').addClass('countdown');
 	var $d = $("<ib/>").addClass('days');
 	var $h = $("<ib/>").addClass('hours');
 	var $m = $("<ib/>").addClass('minutes');
 	var $s = $("<ib/>").addClass('seconds');
-	if( onlySeconds ) {
+	var $splitD = $('<ib />').addClass('countdown-split-days').html('&nbsp;days&nbsp;');
+	var $splitH = $('<ib />').addClass('countdown-split-hours').text(':');
+	var $splitM = $('<ib />').addClass('countdown-split-minutes').text(':');
+	var $splitS = $('<ib />').addClass('countdown-split-seconds').text('').hide();
+	if ( onlySeconds === 'highest') {
+		$splitD.html('d');
+		$splitH.html('h');
+		$splitM.html('m');
+		$splitS.html('s');
+	}
+	if ( onlySeconds === true ) {
 		$countdown.append($s);
 	} else {
 		$countdown.append(
 			$d, 
-			$('<ib />').addClass('countdown-split-days').html('&nbsp;days&nbsp;'), 
+			$splitD,
 			$h,
-			$('<ib />').addClass('countdown-split-hours').text(':'), 
+			$splitH,
 			$m,
-			$('<ib />').addClass('countdown-split-minutes').text(':'), 
-			$s
+			$splitM,
+			$s,
+			$splitS
 			);
 	}
 	update();
+	_self.addSeconds = function(secs) {
+		seconds += secs;
+	};
+	_self.setSeconds = function(secs) {
+		seconds = secs;
+	};
+	_self.resetSeconds = function(secs) {
+		if (secs) seconds = secs;
+		startTime = now();
+	};
 	_self.stop = function() {
 		running = false;
 	};
@@ -39,22 +61,58 @@ function Countdown(seconds, endText, onlySeconds, hideWhenZero, callback) {
 	_self.getJQ = function() {
 		return $countdown;
 	};
+	_self.setSplitterHtml = function(dd,hh,mm,ss) {
+		if(typeof dd !== 'undefined') $splitD.html(dd);
+		if(typeof hh !== 'undefined') $splitH.html(hh);
+		if(typeof mm !== 'undefined') $splitM.html(mm);
+		if(typeof ss !== 'undefined') $splitS.html(ss);
+	};
 	function now() {
 		return new Date().getTime() / 1000;
 	}
 	function update() {
-		if( !running || paused ) {
+		if (!running || paused) {
 			return;
 		}
 		var time = new Date().getTime() / 1000;
 		var showSeconds = seconds - parseInt((time - startTime));
-		if( showSeconds <= 0 ) {
+		if (showSeconds <= 0) {
 			$countdown.html('' + endText);
-			if( callback ) callback();
+			if( callback ) callback(_self);
 			return;
 		}
-		if( onlySeconds ) {
-			$s.html('' + showSeconds);
+		style(showSeconds);
+		setTimeout(update, 1000);
+	}
+	function style (showSeconds) {
+		if (onlySeconds === 'highest') {
+			var s = showSeconds % 60;
+			var m = (showSeconds - s) / 60 % 60;
+			var h = (showSeconds - s - (m * 60)) / 3600 % 24;
+			var d = (showSeconds - s - (m * 60) - (h * 3600)) / 86400;
+			$d.hide();
+			$h.hide();
+			$m.hide();
+			$s.hide();
+			$splitD.hide();
+			$splitH.hide();
+			$splitM.hide();
+			$splitS.hide();
+			if ( d > 0 ) {
+				$d.html(d).show();
+				$splitD.show();
+			} else if ( h > 0 ) {
+				$h.html(h).show();
+				$splitH.show();
+			} else if ( m > 0 ) {
+				$h.html(m).show();
+				$splitM.show();
+			} else if ( s > 0 ) {
+				$h.html(s).show();
+				$splitS.show();
+			}
+		} else if (onlySeconds) {
+			return $s.html('' + showSeconds);
 		} else {
 			var s = showSeconds % 60;
 			var m = (showSeconds - s) / 60 % 60;
@@ -64,13 +122,12 @@ function Countdown(seconds, endText, onlySeconds, hideWhenZero, callback) {
 			$m.html(moreDigits(m,2));
 			$h.html(moreDigits(h,2));
 			$d.html(d + '');
-			if( hideWhenZero ) {
-				if(m <= 0 ) $m.hide().next().hide();
-				if(h <= 0 ) $h.hide().next().hide();
-				if(d <= 0 ) $d.hide().next().hide();
+			if (hideWhenZero) {
+				if (m <= 0 ) $m.hide().next().hide();
+				if (h <= 0 ) $h.hide().next().hide();
+				if (d <= 0 ) $d.hide().next().hide();
 			}
 		}
-		setTimeout(update, 1000);
 	}
 	function moreDigits(num,digits){
 		digits = digits || 2;
